42dbb9ce1996ad199c88eeded2c84e9b46592472
Juan Pablo <jplandi@utpl.edu.ec>
2023-10-23 20:19:43 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto.02/src/proyecto/pkg02/Proyecto02.java
@@ -0,0 +1,7 @@
+package proyecto.pkg02;
+public class Proyecto02 {
+    public static void main(String[] args) {System.out.println("Ejemplo dos básico de java versionado a través de git manejado desde GitHub además se user GitHubClassroom");
+    
+    }
+    
+}
