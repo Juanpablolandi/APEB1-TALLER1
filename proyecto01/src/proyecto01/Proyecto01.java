15ad352f5eac0a1f2dda4b86418e8aca73ff11b7
Juan Pablo <jplandi@utpl.edu.ec>
2023-10-23 19:54:53 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,7 @@
+package proyecto01;
+public class Proyecto01 {
+    public static void main(String[] args) {
+        System.out.println("ejemplo básico de java versionado a través de Git manejado desde GitHub");
+    }
+    
+}
